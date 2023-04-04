76cdcc8c6d6f5ed5a116ed971813222fb36f02d0
Reza Barzergar Gashti <101470327+RezaBG@users.noreply.github.com>
2023-04-04 21:57:05 +0200

    Add files

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+} = honda;
+
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+} = tesla;
+
+const {
+  coloursByPopularity: [hondaTopColor],
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColor],
+} = tesla;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Color</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColor}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColor}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
